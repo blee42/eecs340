4f5252265efabc460639029e63ad0e0647721f2d
aspin <tsunami70875@gmail.com>
2015-01-19 16:29:46 -0600

    almost working

--- src/apps/http_client.cc
+++ src/apps/http_client.cc
@@ -38,20 +38,23 @@ int main(int argc, char * argv[]) {
     server_port = atoi(argv[3]);
     server_path = argv[4];
 
-
-
     /* initialize minet */
     if (toupper(*(argv[1])) == 'K') { 
 	minet_init(MINET_KERNEL);
     } else if (toupper(*(argv[1])) == 'U') { 
 	minet_init(MINET_USER);
     } else {
-	fprintf(stderr, "First argument must be k or u\n");
+ 	fprintf(stderr, "First argument must be k or u\n");
 	exit(-1);
     }
+    //fprintf(wheretoprint, "path: %s", &server_path);
+
+    // create request
+    req = "GET /index.html HTTP/1.0\r\n\r\n";
+    // strcat(req, " HTTP/1.0\r\n\r\n");
 
     /* create socket */
-    sock = new socket(AF_INET, SOCK_STREAM, 0);
+    sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == -1)
       return sock;
 
@@ -62,27 +65,48 @@ int main(int argc, char * argv[]) {
       close(sock);
       return -1;
     }
-
     
     /* set address */
     memset(&sin, 0, sizeof(sin));
     sin.sin_family=AF_INET;
-    sin.sin_port=htons(port);
-    sin.sin_addr.s_addr = *(unsigned long *) host->host_addr_list[0];
+    sin.sin_port=htons(server_port);
+    sin.sin_addr.s_addr = *(unsigned long *) host->h_addr_list[0];
 
     /* connect socket */
-    if (connect(sock, (struct sockaddr *) &sin, sizeof(sin)) != 0 {
+    if (connect(sock, (struct sockaddr *) &sin, sizeof(sin)) != 0) {
       close(sock);
       return -1;
     }
-    
     /* send request */
-
+    datalen = send(sock, req, strlen(req)+1, 0);
+    if (datalen == -1) {
+      close(sock);
+      return -1;
+    }
+    fprintf(wheretoprint, "sended socket\n");
     /* wait till socket can be read */
     /* Hint: use select(), and ignore timeout for now. */
-    
+    //int status = select(1, &set, NULL, NULL, NULL);i
+    int socket_fd, status;
+    do {
+      FD_ZERO(&set);
+      FD_SET(socket_fd, &set);
+      status = select(socket_fd + 1, &set, NULL, NULL, NULL);
+    } while (status == -1);
+    if (status == -1) {
+      close(sock);
+      return -1;
+    }
+    fprintf(wheretoprint, "selected socket?\n");
+
     /* first read loop -- read headers */
-    
+    datalen = read(sock, &buf, BUFSIZE);
+    if (datalen == -1) {
+      close(sock);
+      return -1;
+    }
+    fprintf(wheretoprint, "%s\n", buf);
+   
     /* examine return code */   
     //Skip "HTTP/1.0"
     //remove the '\0'
@@ -93,7 +117,7 @@ int main(int argc, char * argv[]) {
     /* second read loop -- print out the rest of the response */
     
     /*close socket and deinitialize */
-
+    close(sock);
 
     if (ok) {
 	return 0;
