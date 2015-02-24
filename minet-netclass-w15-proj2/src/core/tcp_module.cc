0ada36e1bc20278f093b7523f5b40daa35a6d2aa
Brittany Lee <BrittanyLee2015@u.northwestern.edu>
2015-02-24 11:00:17 -0600

    added comments

--- minet-netclass-w15-proj2/src/core/tcp_module.cc
+++ minet-netclass-w15-proj2/src/core/tcp_module.cc
@@ -23,57 +23,94 @@ using std::string;
 
 int main(int argc, char *argv[])
 {
-  MinetHandle mux, sock;
+    MinetHandle mux, sock;
 
-  MinetInit(MINET_TCP_MODULE);
+    MinetInit(MINET_TCP_MODULE);
 
-  mux=MinetIsModuleInConfig(MINET_IP_MUX) ? MinetConnect(MINET_IP_MUX) : MINET_NOHANDLE;
-  sock=MinetIsModuleInConfig(MINET_SOCK_MODULE) ? MinetAccept(MINET_SOCK_MODULE) : MINET_NOHANDLE;
+    mux=MinetIsModuleInConfig(MINET_IP_MUX) ? MinetConnect(MINET_IP_MUX) : MINET_NOHANDLE;
+    sock=MinetIsModuleInConfig(MINET_SOCK_MODULE) ? MinetAccept(MINET_SOCK_MODULE) : MINET_NOHANDLE;
 
-  if (MinetIsModuleInConfig(MINET_IP_MUX) && mux==MINET_NOHANDLE) {
-    MinetSendToMonitor(MinetMonitoringEvent("Can't connect to mux"));
-    return -1;
-  }
-
-  if (MinetIsModuleInConfig(MINET_SOCK_MODULE) && sock==MINET_NOHANDLE) {
-    MinetSendToMonitor(MinetMonitoringEvent("Can't accept from sock module"));
-    return -1;
-  }
-
-  MinetSendToMonitor(MinetMonitoringEvent("tcp_module handling TCP traffic"));
-
-  MinetEvent event;
-
-  while (MinetGetNextEvent(event)==0) {
-    // if we received an unexpected type of event, print error
-    if (event.eventtype!=MinetEvent::Dataflow 
-	|| event.direction!=MinetEvent::IN) {
-      MinetSendToMonitor(MinetMonitoringEvent("Unknown event ignored."));
-    // if we received a valid event from Minet, do processing
-    } else {
-      //  Data from the IP layer below  //
-      if (event.handle==mux) {
-	Packet p;
-	MinetReceive(mux,p);
-	unsigned tcphlen=TCPHeader::EstimateTCPHeaderLength(p);
-	cerr << "estimated header len="<<tcphlen<<"\n";
-	p.ExtractHeaderFromPayload<TCPHeader>(tcphlen);
-	IPHeader ipl=p.FindHeader(Headers::IPHeader);
-	TCPHeader tcph=p.FindHeader(Headers::TCPHeader);
-
-	cerr << "TCP Packet: IP Header is "<<ipl<<" and ";
-	cerr << "TCP Header is "<<tcph << " and ";
-
-	cerr << "Checksum is " << (tcph.IsCorrectChecksum(p) ? "VALID" : "INVALID");
-	
-      }
-      //  Data from the Sockets layer above  //
-      if (event.handle==sock) {
-	SockRequestResponse s;
-	MinetReceive(sock,s);
-	cerr << "Received Socket Request:" << s << endl;
-      }
+    if (MinetIsModuleInConfig(MINET_IP_MUX) && mux==MINET_NOHANDLE) 
+    {
+        MinetSendToMonitor(MinetMonitoringEvent("Can't connect to mux"));
+        return -1;
     }
-  }
-  return 0;
+
+    if (MinetIsModuleInConfig(MINET_SOCK_MODULE) && sock==MINET_NOHANDLE) 
+    {
+        MinetSendToMonitor(MinetMonitoringEvent("Can't accept from sock module"));
+        return -1;
+    }
+
+    MinetSendToMonitor(MinetMonitoringEvent("tcp_module handling TCP traffic"));
+
+    MinetEvent event;
+
+    while (MinetGetNextEvent(event)==0) 
+    {
+        // if we received an unexpected type of event, print error
+        if (event.eventtype!=MinetEvent::Dataflow || event.direction!=MinetEvent::IN) 
+        {
+            MinetSendToMonitor(MinetMonitoringEvent("Unknown event ignored."));
+        } 
+
+        // else if we received a valid event from Minet, do processing
+        else 
+        {
+            //  Data from the IP layer below  //
+            if (event.handle==mux) 
+            {
+                Packet p;
+                MinetReceive(mux,p);
+                unsigned tcphlen=TCPHeader::EstimateTCPHeaderLength(p);
+                cerr << "estimated header len="<<tcphlen<<"\n";
+                p.ExtractHeaderFromPayload<TCPHeader>(tcphlen);
+                IPHeader ipl=p.FindHeader(Headers::IPHeader);
+                TCPHeader tcph=p.FindHeader(Headers::TCPHeader);
+
+                cerr << "TCP Packet: IP Header is "<<ipl<<" and ";
+                cerr << "TCP Header is "<<tcph << " and ";
+
+                cerr << "Checksum is " << (tcph.IsCorrectChecksum(p) ? "VALID" : "INVALID");
+
+                // TODO: check for correct checksum
+                // TODO: find the info to send responses to (header info, sourceIP, etc.)
+                // TODO: build packet
+                // TODO: send response packet
+
+            }
+
+            //  Data from the Sockets layer above  //
+            if (event.handle==sock) 
+            {
+                SockRequestResponse s;
+                MinetReceive(sock,s);
+                cerr << "Received Socket Request:" << s << endl;
+
+                switch(s.type)
+                {
+                    case CONNECT:
+                    case ACCEPT:
+                        // TODO: create and send response that connection is ok
+                    case STATUS:
+                        // no response needed
+                        break;
+                    case WRITE:
+                        // TODO: build new packet
+                        // TODO: add header to packet (IP and TCP)
+                        // TODO: send packet and request a response
+                        // TODO:: send response request??
+                    case FORWARD:
+                        // TODO: find connection of request
+                        // TODO: request response to that connection?
+                    case CLOSE:
+                        // TODO: find connection of request
+                        // TODO: create and send request 
+                    default: 
+                        // TODO: resend request
+                }
+            }
+        }
+    }
+    return 0;
 }
