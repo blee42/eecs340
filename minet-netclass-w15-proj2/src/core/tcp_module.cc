23022f7bbbe0e65e1626316e3383de2459b50f36
Brittany Lee <BrittanyLee2015@u.northwestern.edu>
2015-02-25 17:02:53 -0600

    quickie

--- minet-netclass-w15-proj2/src/core/tcp_module.cc
+++ minet-netclass-w15-proj2/src/core/tcp_module.cc
@@ -352,7 +352,7 @@ int main(int argc, char *argv[])
             cerr << "\n===ACCEPT===\n";
             
             // unsigned int init_seq_n = rand();
-            TCPState accept_conn(rand(), LISTEN, MAX_TRIES);
+            // TCPState accept_conn(rand(), LISTEN, MAX_TRIES);
             // add window size - "N" value
             // may need to change timeout time
             ConnectionToStateMapping<TCPState> new_conn(req.connection, Time(), accept_conn, false);
