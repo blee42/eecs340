ab9b6fadece1626b390a28d03f6f568ac190d70c
Brittany Lee <BrittanyLee2015@u.northwestern.edu>
2015-02-28 22:05:36 -0600

    quickie

--- minet-netclass-w15-proj2/src/core/tcp_module.cc
+++ minet-netclass-w15-proj2/src/core/tcp_module.cc
@@ -641,7 +641,6 @@ int main(int argc, char *argv[])
             Buffer data;
             while(inflight_n < GBN && (rwnd > 0) && (cwnd > 0))
             {
-              cerr << "packet: " << cs.Print << endl;
               // if MSS < rwnd and MSS < cwnd
               // space in rwnd and cwnd
               if(MSS < rwnd && MSS < cwnd)
