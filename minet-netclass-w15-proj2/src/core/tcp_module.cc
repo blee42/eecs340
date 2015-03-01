956f247758d6a9e2d0aa6369be838a8c7bf4c710
Brittany Lee <BrittanyLee2015@u.northwestern.edu>
2015-02-28 19:05:25 -0600

    quickie

--- minet-netclass-w15-proj2/src/core/tcp_module.cc
+++ minet-netclass-w15-proj2/src/core/tcp_module.cc
@@ -563,7 +563,7 @@ int main(int argc, char *argv[])
           SET_SYN(send_flag);
           Packet send_pack = MakePacket(Buffer(NULL, 0), new_conn.connection, rand(), 0, SEND_BUF_SIZE(new_conn.state), send_flag); // not sure what the seq_n should be
           MinetSend(mux, send_pack);
-          Packet send_pack = MakePacket(Buffer(NULL, 0), new_conn.connection, rand(), 0, SEND_BUF_SIZE(new_conn.state), send_flag); // not sure what the seq_n should be
+          send_pack = MakePacket(Buffer(NULL, 0), new_conn.connection, rand(), 0, SEND_BUF_SIZE(new_conn.state), send_flag); // not sure what the seq_n should be
           MinetSend(mux, send_pack);
 
           cerr << "\n=== SOCK: END CONNECT ===\n";
