4737230331f83d4fb9f98b2007e312d79e64ba44
aspin <tsunami70875@gmail.com>
2015-03-15 16:41:33 -0500

    quickie

--- routelab-w15/table.cc
+++ routelab-w15/table.cc
@@ -63,8 +63,8 @@ void Table::EditEntry(unsigned dest, Entry new_entry) {
 ostream & Table::Print(ostream &os) const 
 {
   os << "Table(rows={";
-  for (deque<Row>::const_iterator i = m.begin(); i != m.end(); i++) { 
-    os << (*i) << ", ";
+  for (deque<Entry>::const_iterator entry = contents.begin(); entry != contents.end(); entry++) { 
+    os << (*entry) << ", ";
   }
   os << "})";
   return os;
