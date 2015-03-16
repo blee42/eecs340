d110fc5ee4878c6f28916865d15b4384328f4c6e
aspin <tsunami70875@gmail.com>
2015-03-15 22:41:59 -0500

    quickie

--- routelab-w15/node.cc
+++ routelab-w15/node.cc
@@ -294,7 +294,7 @@ void Node::UpdatesFromNeighbors()
     {
       // should be no way this is null...
       cerr << "ITERATING: " << endl;
-      cerr << *table.GetEntry((*neighbor)->GetNumber()) << endl;
+      cerr << (*neighbor)->GetNumber() << endl;
       // double neighbor_cost = table.GetEntry((*neighbor)->GetNumber())->cost;
       // cerr << 'Neighbor Cost: ' << neighbor_cost << endl;
       // Entry* neighbor_to_dest = (*neighbor)->GetRoutingTable()->GetEntry(entry->dest_node);
